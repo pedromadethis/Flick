9b93bb81b58b503fe6268529d15b48eb770df096
Chris Wheatley <chriswheatley87@gmail.com>
2014-06-26 21:30:40 +0100

    constants module

---
+++ app/constants.js
@@ -0,0 +1,11 @@
+(function(){
+  'use strict';
+
+  angular.module('flick.constants', [])
+
+  .constant('PATH', {
+    image: 'http://placehold.it/',
+    video: '//player.vimeo.com/video/'
+  })
+
+})();
