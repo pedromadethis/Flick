c04389401b99c01325e27be9ef8736bd112ef217
Chris Wheatley <chriswheatley87@gmail.com>
2014-06-29 15:08:20 +0100

    disable hashbangs

--- app/app.js
+++ app/app.js
@@ -8,12 +8,14 @@
    'angular-carousel'
    ])
 
-  .config(function ($routeProvider, $sceProvider) {
+  .config(function ($routeProvider, $locationProvider, $sceProvider) {
 
     $routeProvider
       .otherwise({
         redirectTo: '/'
       });
+
+    $locationProvider.html5Mode(true);
 
     // @fixme - remove and use $sce.trustAsResourceUrl();
     $sceProvider.enabled(false);
