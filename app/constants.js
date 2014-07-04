ee5420d4578a7e5f5ddca3856249c308e20bf43b
Chris Wheatley <chriswheatley87@gmail.com>
2014-07-04 22:33:03 +0100

    constantise query string for video iframe url

--- app/constants.js
+++ app/constants.js
@@ -5,7 +5,8 @@
 
   .constant('PATH', {
     image: 'assets/img/',
-    video: '//player.vimeo.com/video/'
+    video: '//player.vimeo.com/video/',
+    querystring: '?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=fff&amp;autoplay=1'
   });
 
 })();
